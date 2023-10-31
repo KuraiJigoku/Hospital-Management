776cdd4464e92818dfe1191b6611ceec51098b1f
MrHarryKenobi <niranjangnair3@gmail.com>
2023-10-31 21:49:27 +0530

    bob

--- main.py
+++ main.py
@@ -2,26 +2,24 @@ import prequisite
 import management.admin as admin
 from texttable import Texttable
 t1=Texttable()
-t1.set_cols_align(['c'])
+t1.set_cols_align(['l'])
 t1.set_cols_valign(['m'])
 t1.add_rows([['WELCOME TO RADCLIFF HOSPITAL MANAGEMENT SYSTEM'],
-             [''],
              ['1. Login as ADMIN'],
              ['2. Register as USER'],
              ['3. Login as USER']
              ])
-t1.draw()
+print(t1.draw())
 v=int(input('Enter your Choice: '))
 if v==1:
     t2=Texttable()
-    t2.set_cols_align(['c'])
+    t2.set_cols_align(['l'])
     t2.set_cols_valign(['m'])
     t2.add_rows([['LOGGED IN AS ADMIN'],
-                 [''],
                  ['1. Doctors Details'],
                  ['2. Patient Details'],
                  ['3. Exit']
                  ])
-    t2.draw()
+    print(t2.draw())
     c=int(input('Enter Choice: '))
     
--- management/admin.py
+++ management/admin.py
@@ -46,22 +46,21 @@ def rdoctor():
 def udoctor():
     try:
         t3=Texttable()
-        t3.set_cols_align(['c'])
+        t3.set_cols_align(['l'])
         t3.set_cols_valign(['m'])
         t3.add_rows([['LOGGED IN AS ADMIN'],
-                     [''],
                      ['1. Update NAME'],
                      ['2. Update DEPARTMENT'],
                      ['3. Update FEES'],
                      ['4. Update ROOM No.']
                      ])
-        t3.draw()
+        print(t3.draw())
         c=int(input('Enter Choice: '))
         if c==1:
             id=int(input('Enter Doctor ID: '))
             name=input('Enter Update Doctor Name: ')
             v=name,id
-            q='update doctor set name=%s where id=%s'
+            q='update doctor set name=%s where did=%s'
             cur.execute(q,v)
             con.commit()
             print('Name Updated')
@@ -69,7 +68,7 @@ def udoctor():
             id=int(input('Enter Doctor ID: '))
             dept=input('Enter Update Doctor Department: ')
             v=dept,id
-            q='update doctor set dept=%s where id=%s'
+            q='update doctor set dept=%s where did=%s'
             cur.execute(q,v)
             con.commit()
             print('Department Updated')
@@ -77,7 +76,7 @@ def udoctor():
             id=int(input('Enter Doctor ID: '))
             fees=input('Enter Update Doctor Fees: ')
             v=fees,id
-            q='update doctor set fees=%s where id=%s'
+            q='update doctor set fees=%s where did=%s'
             cur.execute(q,v)
             con.commit()
             print('Fees Updated')
@@ -85,7 +84,7 @@ def udoctor():
             id=int(input('Enter Doctor ID: '))
             room=input('Enter Update Doctor Room no.: ')
             v=dept,id
-            q='update doctor set room=%s where id=%s'
+            q='update doctor set room=%s where did=%s'
             cur.execute(q,v)
             con.commit()
             print('Room No. Updated')
@@ -93,7 +92,7 @@ def udoctor():
         print(E.msg)
 def ddoctor():
     try:
-        q='delete from doctor where id=%s'
+        q='delete from doctor where did=%s'
         id=int(input('Enter Doctor ID: '))
         v=(id,)
         cur.execute(q,v)
@@ -102,63 +101,64 @@ def ddoctor():
         print(E.msg)
 def disdoctor():
     try:
-        q='select * from doctor'
+        q="select * from doctor"
         cur.execute(q)
         t4=Texttable()
-        t4.set_cols_align(['c','c','c','c','c',])
-        t4.set_cols_valign(['m'])
-        t4.add_rows([['DID','NAME','DEPT','FEES','ROOM No.']])
+        t4.set_cols_align(["c","c","c","c","c"])
+        t4.set_cols_valign(["m","m","m","m","m"])
+        t4.set_cols_dtype(["i","t","t","i","i"])
+        t4.add_row(["DID","NAME","DEPT","FEES","ROOM No."])
         r=cur.fetchall()
         for x in r:
-            t4.addrow([x])
-        t4.draw()
-        
+            t4.add_row(x)
+        print(t4.draw())
+    except myc.Error as E:
+        print(E.msg)
 def upatient():
     cur.execute('CREATE TABLE IF NOT EXISTS patient(PID int,USERNAME varchar(15),NAME varchar(20),AGE int,BLOOD_GROUP varchar(3),DID int,PRIMARY KEY (PID),FOREIGN KEY (DID) references doctor(DID)),FOREIGN KEY (USERNAME) references login(USERNAME))')
     try:
         t5=Texttable()
-        t5.set_cols_align(['c'])
+        t5.set_cols_align(['l'])
         t5.set_cols_valign(['m'])
         t5.add_rows([['LOGGED IN AS ADMIN'],
-                     [''],
-                     ['1. Update PATIENT'],
-                     ['2. Update DEPARTMENT'],
-                     ['3. Update FEES'],
+                     ['1. Update NAME'],
+                     ['2. Display AGE'],
+                     ['3. Update BLOOD GROUP'],
                      ['4. Update ROOM No.']
                      ])
-        t5.draw()
+        print(t5.draw())
         c=int(input('Enter Choice: '))
         if c==1:
-            id=int(input('Enter Doctor ID: '))
-            name=input('Enter Update Doctor Name: ')
+            id=int(input('Enter Patient ID: '))
+            name=input('Enter Update Patient Name: ')
             v=name,id
-            q='update doctor set name=%s where id=%s'
+            q='update patient set name=%s where pid=%s'
             cur.execute(q,v)
             con.commit()
             print('Name Updated')
         if c==2:
-            id=int(input('Enter Doctor ID: '))
-            dept=input('Enter Update Doctor Department: ')
+            id=int(input('Enter Patient ID: '))
+            dept=input('Enter Update Patient Age: ')
             v=dept,id
-            q='update doctor set dept=%s where id=%s'
+            q='update patient set age=%s where pid=%s'
             cur.execute(q,v)
             con.commit()
-            print('Department Updated')
+            print('Age Updated')
         if c==3:
-            id=int(input('Enter Doctor ID: '))
-            fees=input('Enter Update Doctor Fees: ')
+            id=int(input('Enter Patient ID: '))
+            fees=input('Enter Update Patient Blood Group: ')
             v=fees,id
-            q='update doctor set fees=%s where id=%s'
+            q='update doctor set blood_group=%s where pid=%s'
             cur.execute(q,v)
             con.commit()
-            print('Fees Updated')
+            print('Blood Group Updated')
         if c==4:
-            id=int(input('Enter Doctor ID: '))
-            room=input('Enter Update Doctor Room no.: ')
+            id=int(input('Enter Patient ID: '))
+            room=input('Enter Update Patient Room No.: ')
             v=dept,id
-            q='update doctor set room=%s where id=%s'
+            q='update doctor set room=%s where pid=%s'
             cur.execute(q,v)
             con.commit()
             print('Room No. Updated')
     except mycc.Error as E:
-        print(E.msg)    
+        print(E.msg)
