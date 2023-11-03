dfc98fb3ff83685d8bb4ddf37953ec7b059eeeb5
MrHarryKenobi <niranjangnair3@gmail.com>
2023-11-03 18:44:50 +0530

    Updates again -.-

--- mysqlc.py
+++ mysqlc.py
@@ -1,9 +1,14 @@
+
 import mysql.connector as myc
 host=input('Enter MySQL server hostname: ')
 user=input('Enter MySQL Username: ')
 pas=input('Enter MySQL Password: ')
 
 def connection():
+    """
+    Establishes a connection to the MySQL server and creates a database if it doesn't exist.
+    Returns a tuple containing the connection and cursor objects.
+    """
     try:
         con=myc.connect(host=host,user=user,password=pas)
         cur=con.cursor()
