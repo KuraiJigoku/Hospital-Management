d6a52a053fae969fb164dbdaed28181359027eb0
MrHarryKenobi <niranjangnair3@gmail.com>
2023-12-20 15:50:40 +0530

    bob

--- management/admin.py
+++ management/admin.py
@@ -53,9 +53,9 @@ def adoctor():
         print(E.msg)
 def rdoctor():
     try:
-        id=int(input('Enter Doctor ID to Remove from Database'))
-        v=(id,)
+        id=int(input('Enter Doctor ID to Remove from Database: '))
         q='delete from doctor where id=%s'
+        v=(id,)
         cur.execute(q,v)
     except myc.Error as E:
         print(E.msg)
